ff722ad86fb83e628983dbc2007b3baad753dd16
Kiran Biradar <116005212+KiruOP@users.noreply.github.com>
2024-07-26 11:29:30 +0530

    added client code

---
+++ client/src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import Allroutes from "../src/Allroutes";
+import Navbar from "./Component/Navbar/Navbar";
+
+import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Videoupload from "./Pages/Videoupload/Videoupload";
+import Createeditchannel from "./Pages/Channel/Createeditchannel";
+import Drawersliderbar from "../src/Component/Leftsidebar/Drawersliderbar";
+
+import { getallvideo } from "./action/video";
+import { getallcomment } from "./action/comment";
+import { getallhistory } from "./action/history";
+import { fetchallchannel } from "./action/channeluser";
+import { getalllikedvideo } from "./action/likedvideo";
+import { getallwatchlater } from "./action/watchlater";
+function App() {
+  const [toggledrawersidebar, settogledrawersidebar] = useState({
+    display: "none",
+  });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchallchannel());
+    dispatch(getallvideo());
+    dispatch(getallcomment());
+    dispatch(getallhistory());
+    dispatch(getalllikedvideo());
+    dispatch(getallwatchlater());
+  }, [dispatch]);
+
+  const toggledrawer = () => {
+    if (toggledrawersidebar.display === "none") {
+      settogledrawersidebar({
+        display: "flex",
+      });
+    } else {
+      settogledrawersidebar({
+        display: "none",
+      });
+    }
+  };
+  const [editcreatechanelbtn, seteditcreatechanelbtn] = useState(false);
+  const [videouploadpage, setvideouploadpage] = useState(false);
+  return (
+    <Router>
+      {videouploadpage && (
+        <Videoupload setvideouploadpage={setvideouploadpage} />
+      )}
+      {editcreatechanelbtn && (
+        <Createeditchannel seteditcreatechanelbtn={seteditcreatechanelbtn} />
+      )}
+      <Navbar
+        seteditcreatechanelbtn={seteditcreatechanelbtn}
+        toggledrawer={toggledrawer}
+      />
+      <Drawersliderbar
+        toggledraw={toggledrawer}
+        toggledrawersidebar={toggledrawersidebar}
+      />
+      <Allroutes
+        seteditcreatechanelbtn={seteditcreatechanelbtn}
+        setvideouploadpage={setvideouploadpage}
+      />
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose } from "redux";
+import { legacy_createStore as createstore } from "redux";
+import { thunk } from "redux-thunk";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Reducers from "./Reducers";
+const store = createstore(Reducers, compose(applyMiddleware(thunk)));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <GoogleOAuthProvider clientId="490105890299-0j7jegid2plfubvgb3kqrio79g8rghet.apps.googleusercontent.com">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </GoogleOAuthProvider>
+  </Provider>
+);
